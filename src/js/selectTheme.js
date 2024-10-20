f1a0c7344667a17036413fa160c32063a0e3458a
ovik <ovik328@gmail.com>
2024-10-20 14:59:21 +0300

    Смена темы сделана на чистом CSS, selectTheme.js удалён

--- src/js/selectTheme.js
+++
@@ -1,6 +0,0 @@
-const selectTheme = document.getElementById("theme");
-
-selectTheme.addEventListener("change", () => {
-    selectTheme.classList.toggle("theme__select--light");
-    selectTheme.classList.toggle("theme__select--dark");
-});
